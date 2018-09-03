8baa8a83f887bf6e168b47a4726ab29126aff80a
Abhishek Paul <abhishekp@digitalaptech.com>
2018-09-03 14:51:37 +0530

    Implement token verification endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ const express = require('express')
 
 let UserModel = require('../models/user.model');
 
-  router.post('/login',
+router.post('/login',
   global.middlewares.logBodyAndUploadedFile,
   (req, res, next) => {
 
@@ -39,15 +39,33 @@ let UserModel = require('../models/user.model');
           data: null,
         });
       }      
-  })
-  .catch((err) => {
-    console.log(err);
-    res.status(200).json({
-      status: 0,
-      message: "Some error has occoured. Please try after sometime",
-      data: null,
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(200).json({
+          status: 0,
+          message: "Some error has occoured. Please try after sometime",
+          data: null,
+        });
+      });
+});
+
+router.post('/verifyToken',
+  global.middlewares.logBodyAndUploadedFile,
+  (req,res,next)=> {
+    commonMethods.verifyJWTToken(req.body.token)
+    .then((decodedResult)=>{
+      res.status(200).json({
+        status: 1,
+        message: "Token is valid",
+      });
+    })
+    .catch((err)=>{
+      res.status(200).json({
+        status: 0,
+        message: "Invalid token or token has been expired",
+      });
     });
-  });
 });
 
 //exports this module
